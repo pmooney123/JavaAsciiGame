3dbd44db008af0c4e95e02a1d11f0f7615653f2a
pmooney123 <67892202+pmooney123@users.noreply.github.com>
2021-06-20 20:30:53 -0700

    Add files via upload

---
+++ dinoAscii/src/Creature.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+
+public class Creature {
+    private World world;
+
+    public int x;
+    public int y;
+
+    private CreatureAi ai;
+    public void setCreatureAi(CreatureAi ai) { this.ai = ai; }
+
+    private char glyph;
+    public char glyph() { return glyph; }
+
+    private Color color;
+    public Color color() { return color; }
+
+    public Creature(World world, char glyph, Color color){
+        this.world = world;
+        this.glyph = glyph;
+        this.color = color;
+    }
+    public void dig(int wx, int wy) {
+        world.dig(wx, wy, 0.1);
+    }
+
+    public void moveBy(int mx, int my){
+        Creature other = world.creatureHere(x+mx, y+my);
+
+        if (other == null)
+            ai.onEnter(x+mx, y+my, world.tile(x+mx, y+my));
+        else
+            attack(other);
+    }
+
+    public void attack(Creature other){
+        world.remove(other);
+    }
+
+    public void update(){
+        ai.onUpdate();
+    }
+
+    public boolean canEnter(int wx, int wy) {
+        return world.tile(wx, wy).isGround() && world.creatureHere(wx, wy) == null;
+    }
+}
---
+++ dinoAscii/src/CreatureFactory.java
@@ -0,0 +1,26 @@
+public class CreatureFactory {
+    private World world;
+
+    public CreatureFactory(World world){
+        this.world = world;
+
+    }
+    public Creature newPlayer(){
+        Creature player = new Creature(world, '@', AsciiPanel.brightWhite);
+        world.addAtEmptyLocation(player);
+        new PlayerAi(player);
+        return player;
+    }
+
+    public Creature newFungus(){
+        Creature fungus = new Creature(world, 'P', AsciiPanel.green);
+        world.addAtEmptyLocation(fungus);
+        new FungusAi(fungus);
+
+        return fungus;
+    }
+}
+
+
+
+
